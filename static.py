24d43b34e6a9671672459a319d67fa25ef7dae59
Matt Mowers <Matthew.Mowers@nrel.gov>
2017-06-13 02:48:44 -0400

    Shorten names of presets so they can fit in excel sheet names

--- static.py
+++ static.py
@@ -5,9 +5,9 @@ Static HTML report maker
 from core import reeds_static
 
 static_presets = [
-    {'result': 'Capacity (GW)', 'presets': ['Stacked Capacity', 'State Map: 2050 Wind']},
+    {'result': 'Capacity (GW)', 'presets': ['Stacked Capacity', 'State Map 2050 Wind']},
     {'result': 'New Capacity (GW)', 'presets': ['Stacked Capacity']},
-    {'result': 'Generation (TWh)', 'presets': ['Stacked Gen', 'State Map: 2050 Wind']},
+    {'result': 'Generation (TWh)', 'presets': ['Stacked Gen', 'State Map 2050 Wind']},
 ]
 path = r'C:\Users\mmowers\Projects\Model Improvement\Bokeh\reeds_pivot_test'
 reeds_static(path, static_presets)
