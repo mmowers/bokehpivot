d589f550a08f529b924f8c76bf0517a3507f7af0
Matt Mowers <Matthew.Mowers@nrel.gov>
2017-06-30 20:30:57 -0400

    Build standard report

--- standard_report.py
+++ standard_report.py
@@ -4,10 +4,17 @@ Static HTML report maker
 '''
 from core import reeds_static
 
+path = r'C:\Users\mmowers\Projects\Model Improvement\Bokeh\reeds_pivot_test'
 static_presets = [
-    {'result': 'Capacity (GW)', 'presets': ['Stacked Capacity', 'State Map 2050 Wind']},
-    {'result': 'New Capacity (GW)', 'presets': ['Stacked Capacity']},
-    {'result': 'Generation (TWh)', 'presets': ['Stacked Gen', 'State Map 2050 Wind']},
+    {'result': 'Capacity (GW)', 'presets': ['Stacked Bars']},
+    {'result': 'New Capacity (GW)', 'presets': ['Stacked Bars']},
+    {'result': 'Retirements (GW)', 'presets': ['Stacked Bars']},
+    {'result': 'Wind Capacity (GW)', 'presets': ['2050 Map']},
+    {'result': 'Capacity (GW)', 'presets': ['PCA Map 2050 Solar']},
+    {'result': 'Generation (TWh)', 'presets': ['Stacked Bars']},
+    {'result': 'Gen by m (GW)', 'presets': ['Stacked Bars 2050']},
+    {'result': 'Emissions, Fuel, Prices', 'presets': ['CO2 Emissions']},
+    {'result': 'Elec Price (2015$/MWh)', 'presets': ['National Scenario']},
+    {'result': 'System Cost (2015$)', 'presets': ['2016-2050 Stacked Bars']},
 ]
-path = r'\\nrelqnap01d\ReEDS\FY17-ABB Update-MRM-a877bcf-abb_update\runs'
 reeds_static(path, static_presets)
