7912d39fd21e86d64bdc6ada92486a758e24739c
Matt Mowers <Matthew.Mowers@nrel.gov>
2017-07-30 18:23:41 -0400

    Restructure static reports

--- reports/templates/standard_compare_report.py
+++ reports/templates/standard_compare_report.py
@@ -1,9 +1,10 @@
 '''
 Static HTML report maker
 
+To use, copy this file into the implementations/ directory, change path and base, and run
 '''
 import os, sys
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
+sys.path.insert(1, os.path.join(sys.path[0], '../..'))
 import reeds_bokeh as rb
 
 path = r'\\nrelqnap01d\ReEDS\FY17-WindRuns-MRM-d1c8e69-wind_cost_scenarios\runs\20170322_jedi_outputs'
