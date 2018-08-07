d58456a973b2e8d6dc666651c34a16c192f75680
mmowers <Matthew.Mowers@nrel.gov>
2018-08-07 10:33:16 -0600

    Make run_report.py script more flexible and easy to use

--- reports/run_report.py
+++ reports/run_report.py
@@ -1,19 +1,24 @@
+#Copy this file anywhere and run as a python script in a command prompt.
+
+#EDIT THE FOLLOWING FIELDS
+bokehpivot_dir = r'\\nrelqnap02\ReEDS\bokehpivot' #path to the desired bokehpivot repo.
+data_source = r'\\nrelqnap02\ReEDS\Some Project\runs\Some Runs Folder' #data_source allows all the same inputs as in the interface
+scenario_filter = 'all' #'all' or string of comma-separated names.
+base = 'Master' #Base case, if applicable. If base case is not needed for this report, simply leave as is.
+report_path = r'\\nrelqnap02\ReEDS\Some Location\some_report.py' #Path to report that is to be run
+html_num = 'one' #'one' or 'multiple'. 'one' will create one html file with all sections, and 'multiple' will create a separate html file for each section
+output_dir = r'\\nrelqnap02\ReEDS\Some Location' #This is the location that will hold the report.
+auto_open = 'yes' #'yes' or 'no'. Automatically open the resulting report excel and html files when they are created.
+
+#DON'T EDIT BELOW
 import os, sys
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-import reeds_bokeh as rb
 import importlib
 import datetime
-
-data_source = '//nrelqnap02/ReEDS/Some Project/runs/Some Runs Folder'
-scenario_filter = 'all' #'all' or comma separated string
-base = 'Master'
-report_path = '//nrelqnap02/ReEDS/Some Location/report.py'
-html_num = 'one' #'one' or 'multiple'
-output_dir = '//nrelqnap02/ReEDS/Some Location/report-' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-auto_open = 'yes'
-
+sys.path.insert(1, bokehpivot_dir)
+import reeds_bokeh as rb
 report_dir = os.path.dirname(report_path)
 sys.path.insert(1, report_dir)
 report_name = os.path.basename(report_path)[:-3]
 report = importlib.import_module(report_name)
+output_dir = output_dir + '/report-' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 rb.reeds_static(data_source, scenario_filter, base, report.static_presets, report_path, 'both', html_num, output_dir, auto_open)
