5c96ab59f145e51fbb47dcad35886a043d93bace
Matt Mowers <Matthew.Mowers@nrel.gov>
2018-08-08 18:29:55 -0400

    Allow user to change report format in run_report.py

--- reports/run_report.py
+++ reports/run_report.py
@@ -9,6 +9,7 @@ base = 'Master' #Base case, if applicable. If base case is not needed for this r
 report_path = r'\\nrelqnap02\ReEDS\Some Location\some_report.py' #Path to report that is to be run
 html_num = 'one' #'one' or 'multiple'. 'one' will create one html file with all sections, and 'multiple' will create a separate html file for each section
 output_dir = r'\\nrelqnap02\ReEDS\Some Location' #This is the location that will hold the report.
+report_format = 'both' #'html', 'excel', or 'both'
 auto_open = 'yes' #'yes' or 'no'. Automatically open the resulting report excel and html files when they are created.
 
 #DON'T EDIT BELOW HERE
@@ -22,4 +23,4 @@ sys.path.insert(1, report_dir)
 report_name = os.path.basename(report_path)[:-3]
 report = importlib.import_module(report_name)
 output_dir = output_dir + '/report-' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-rb.reeds_static(data_source, scenario_filter, base, report.static_presets, report_path, 'both', html_num, output_dir, auto_open)
+rb.reeds_static(data_source, scenario_filter, base, report.static_presets, report_path, report_format, html_num, output_dir, auto_open)
