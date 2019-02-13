e19029faa2b1526b17bb5e745a53da12132f3221
mmowers <Matthew.Mowers@nrel.gov>
2019-02-13 10:29:37 -0700

    Remove reference to 2015$ in reports

--- reports/templates/standard_report.py
+++ reports/templates/standard_report.py
@@ -5,8 +5,8 @@ static_presets = [
     {'name': 'Retirements (GW)', 'result': 'Retirements (GW)', 'preset': 'Stacked Bars'},
     {'name': 'Final Gen by timeslice (GW)', 'result': 'Gen by m national (GW)', 'preset': 'Stacked Bars Final'},
     {'name': 'CO2 Emissions (MMton)', 'result': 'Emissions, Fuel, Prices', 'preset': 'CO2 Emissions (MMton)'},
-    {'name': 'Elec Price (2015$/MWh)', 'result': 'Elec Price ($/MWh)', 'preset': 'National Scenario'},
-    {'name': 'System Cost (Bil 2015$)', 'result': 'Sys Cost (Bil $)', 'preset': 'Stacked Bars'},
+    {'name': 'Elec Price ($/MWh)', 'result': 'Elec Price ($/MWh)', 'preset': 'National Scenario'},
+    {'name': 'System Cost (Bil $)', 'result': 'Sys Cost (Bil $)', 'preset': 'Stacked Bars'},
     {'name': 'Final Wind Capacity (GW)', 'result': 'Wind Capacity (GW)', 'preset': 'Final Map'},
     {'name': 'Final Solar Capacity (GW)', 'result': 'Capacity (GW)', 'preset': 'PCA Map Final Solar'},
 ]
