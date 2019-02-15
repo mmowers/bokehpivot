8a0beb1a3ca7eba7cdde01206649d0623ee49567
mmowers <Matthew.Mowers@nrel.gov>
2019-02-15 14:55:16 -0700

    Add post-processing section to run_report.py

--- reports/run_report.py
+++ reports/run_report.py
@@ -13,7 +13,7 @@ output_dir = r'\\nrelqnap02\ReEDS\Some Location' #This is the directory that wil
 report_format = 'both' #'html', 'excel', or 'both'
 auto_open = 'yes' #'yes' or 'no'. Automatically open the resulting report excel and html files when they are created.
 
-#DON'T EDIT BELOW HERE
+#DON'T EDIT THIS SECTION
 import os, sys
 import importlib
 import datetime
@@ -24,3 +24,6 @@ sys.path.insert(1, report_dir)
 report_name = os.path.basename(report_path)[:-3]
 report = importlib.import_module(report_name)
 rb.reeds_static(data_type, data_source, scenario_filter, base, report.static_presets, report_path, report_format, html_num, output_dir, auto_open)
+
+#CUSTOM POSTPROCESSING
+#Any post-processing of the excel data that was produced. you can read excel data into dataframes by importing pandas and using pandas.read_excel()
