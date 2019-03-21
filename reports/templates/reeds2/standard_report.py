000d1be721e08508727f882aa6d5dfe1abb3ca2d
mmowers <Matthew.Mowers@nrel.gov>
2019-03-21 16:11:21 -0700

    Refactor price outputs

--- reports/templates/reeds2/standard_report.py
+++ reports/templates/reeds2/standard_report.py
@@ -4,9 +4,9 @@ static_presets = [
     {'name': 'New Annual Capacity (GW)', 'result': 'New Annual Capacity BA (GW)', 'preset': 'Stacked Bars'},
     {'name': 'Annual Retirements (GW)', 'result': 'Annual Retirements BA (GW)', 'preset': 'Stacked Bars'},
     {'name': 'Final Gen by timeslice (GW)', 'result': 'Gen by timeslice national (GW)', 'preset': 'Stacked Bars Final'},
-    {'name': 'National Energy Price ($/MWh)', 'result': 'Energy Price National ($/MWh)', 'preset': 'Scenario Lines Over Time'},
+    {'name': 'National Energy Price ($/MWh)', 'result': 'Requirement Prices and Quantities', 'preset': 'Energy Price Lines ($/MWh)'},
     {'name': 'System Cost (Billion $)', 'result': 'Sys Cost (Bil $)', 'preset': 'Stacked Bars'},
     {'name': 'CO2 Emissions National (MMton)', 'result': 'CO2 Emissions National (MMton)', 'preset': 'Scenario Lines Over Time'},
     {'name': 'Final Wind Capacity (GW)', 'result': 'Capacity Resource Region (GW)', 'preset': 'RR Map Final Wind'},
-    {'name': 'Final Regional Energy Price (2015$/MWh)', 'result': 'Energy Price BA ($/MWh)', 'preset': 'Final BA Map'},
+    {'name': 'Final Regional Energy Price ($/MWh)', 'result': 'Requirement Prices and Quantities', 'preset': 'Energy Price Final BA Map ($/MWh)'},
 ]
