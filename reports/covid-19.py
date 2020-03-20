94f7e2674abfe5b28690be28e9f706a2959c1d58
Matt Mowers <Matthew.Mowers@nrel.gov>
2020-03-20 02:01:01 -0400

    Update COVID-19 report maps

--- reports/covid-19.py
+++ reports/covid-19.py
@@ -8,8 +8,9 @@ states = df_state_code['Code'].tolist()
 static_presets = [
     {'name': 'Cumulative worldwide', 'config':{'x':'days from start', 'y':'number', 'series':'type', 'plot_width':'400', 'x_title':'Days since Jan 22', 'sync_axes':'No'}},
     {'name': 'Cumulative by country', 'config':{'chart_type':'Bar', 'x':'days from start', 'y':'number', 'series':'top countries', 'explode':'type', 'plot_width':'400', 'y_min':'0', 'x_title':'Days since Jan 22', 'sync_axes':'No'}},
-    {'name': 'US cumulative by state past 2 weeks', 'config':{'chart_type':'Bar', 'x':'days ago', 'y':'number', 'series':'us top st', 'explode':'type', 'plot_width':'400', 'y_min':'0', 'x_title':'Days ago', 'sync_axes':'No', 'filter':{'country':['US'], 'days ago':list(range(15))}}},
-    {'name': 'US cumulative deaths state map past 3 days', 'config':{'chart_type':'Area Map', 'x':'st', 'y':'number','explode':'days ago', 'filter':{'days ago':[0,1,2,3], 'country':['US'], 'st':states, 'type':['Deaths']}}},
+    {'name': 'US cumulative by state past two weeks', 'config':{'chart_type':'Bar', 'x':'days ago', 'y':'number', 'series':'us top st', 'explode':'type', 'plot_width':'400', 'y_min':'0', 'x_title':'Days ago', 'sync_axes':'No', 'filter':{'country':['US'], 'days ago':list(range(15))}}},
+    {'name': 'US cumulative infections by state past two days', 'config':{'chart_type':'Area Map', 'x':'st', 'y':'number', 'explode':'days ago', 'filter':{'days ago':[0,1,2], 'country':['US'], 'st':states, 'type':['Confirmed']}}},
+    {'name': 'US cumulative deaths by state past two days', 'config':{'chart_type':'Area Map', 'x':'st', 'y':'number', 'explode':'days ago', 'filter':{'days ago':[0,1,2], 'country':['US'], 'st':states, 'type':['Deaths']}}},
 ]
 
 # format_string = {'plot_width':'600', 'x_title':'Days since Jan 22'}
